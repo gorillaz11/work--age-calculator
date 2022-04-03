42421a6183a94d92d7fdbb7eb600a96848cbd60c
gorillaz11 <102285005+gorillaz11@users.noreply.github.com>
2022-04-03 15:05:16 +0300

    Add files via upload

---
+++ grade-calculator.py
@@ -0,0 +1,10 @@
+grade=int(input("your grade:"))
+
+if grade > 7 and grade<=10 :
+    print("great")
+elif grade>4 and grade<=7:
+    print("ok!")
+elif grade>=1 and grade<=4:
+    print("BAD ")
+elif grade<1 and grade<=11:
+    print("doesnt exist")
